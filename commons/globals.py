e300973129892f879ca4be42de3c018255a46ad3
ron elkayam <ronelk6@gmail.com>
2025-08-05 10:06:44 +0300

    move user insensitive to env file

--- commons/globals.py
+++ commons/globals.py
@@ -1,19 +1,26 @@
-URL = "https://www.mysql.com/"
-BROWSER = 1  # 1 - chrome , 2 - Firefox
-USER_DETAILS = {"email": 'test@gmail.com',
-                "password": 'AaBbCc12!@',
-                "first_name": 'test',
-                "last_name": 'qa',
-                "job": 'QA',
-                "phone": '054-112233',
-                "company_name": 'company',
-                "address": 'any address',
-                "city": 'city',
-                "zip": '123456'
+from dotenv import load_dotenv
+import os
 
-                }
+load_dotenv()
 
-EXIST_USER = "dkdk100@walla.co.il"
-EXIST_PASSWORD = "YaaledIm_55"
-REF_MANUAL_VERSION = "8.4"
-RELEASE_NOTE_VERSION = "8.4"
+URL = os.getenv("URL")
+BROWSER = int(os.getenv("BROWSER", 1))  # ברירת מחדל ל־1 אם לא מוגדר
+
+USER_DETAILS = {
+    "email": os.getenv("EMAIL"),
+    "password": os.getenv("PASSWORD"),
+    "first_name": os.getenv("FIRST_NAME"),
+    "last_name": os.getenv("LAST_NAME"),
+    "job": os.getenv("JOB"),
+    "phone": os.getenv("PHONE"),
+    "company_name": os.getenv("COMPANY_NAME"),
+    "address": os.getenv("ADDRESS"),
+    "city": os.getenv("CITY"),
+    "zip": os.getenv("ZIP")
+}
+
+EXIST_USER = os.getenv("EXIST_USER")
+EXIST_PASSWORD = os.getenv("EXIST_PASSWORD")
+
+REF_MANUAL_VERSION = os.getenv("REF_MANUAL_VERSION")
+RELEASE_NOTE_VERSION = os.getenv("RELEASE_NOTE_VERSION")
